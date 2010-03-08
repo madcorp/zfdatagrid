43625ba5193d73b56e5a26481a3019304adcc819
James Solomon <solomonjames@users.noreply.github.com>
2010-03-08 20:26:47 +0000

    Rearrange

---
+++ Bvb/Grid/ExtraColumns.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license
+ * It is  available through the world-wide-web at this URL:
+ * http://www.petala-azul.com/bsd.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to geral@petala-azul.com so we can send you a copy immediately.
+ *
+ * @package    Bvb_Grid
+ * @copyright  Copyright (c)  (http://www.petala-azul.com)
+ * @license    http://www.petala-azul.com/bsd.txt   New BSD License
+ * @version    $Id: ExtraColumns.php 538 2010-02-08 17:47:01Z pao.fresco@gmail.com $
+ * @author     Bento Vilas Boas <geral@petala-azul.com >
+ */
+
+
+
+class Bvb_Grid_ExtraColumns
+{
+    public   $_field;
+
+    function __call($name,$args)
+    {
+        $this->_field[$name] = $args[0];
+        return $this;
+    }
+
+}
