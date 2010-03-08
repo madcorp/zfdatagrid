852a9d734656c750008981ad595efc26948502f1
solomonjames <solomonjames@0f8c3b7c-9498-11dd-88c4-d55280a115f2>
2010-03-08 20:49:26 +0000

    Base

--- branches/solomonjames/scripts/doctrine-cli.php
+++ branches/solomonjames/scripts/doctrine-cli.php
@@ -8,9 +8,7 @@
     require_once AP.'/../library/Doctrine/Doctrine/Parser/sfYaml/sfYaml.php';
     
     // bootstrap Doctrine and the Autoloader
-    $application = Cb_Application::getInstance()->getApplication();
-    $application->getBootstrap()
-        ->bootstrap('autoload');
+    $application->bootstrap('autoload');
 
     // set aggressive loading to make sure migrations are working
     Doctrine_Manager::getInstance()->setAttribute(
@@ -23,11 +21,6 @@
     $options = $application->getBootstrap()->getOptions();
     
     Doctrine_Manager::getInstance()->openConnection($options['resources']['doctrine']['master'], 'master');
-    Doctrine_Manager::getInstance()->openConnection($options['resources']['doctrine']['dt'], 'dt');
-    
-    $cb_generate_yaml_db_path = dirname(dirname(__FILE__)) . '/library/Cb/Doctrine/Task/CbGenerateYamlDb.php';
-    require $cb_generate_yaml_db_path;
     
     $cli = new Doctrine_Cli($options['resources']['doctrine']);
-    $cli->registerTaskClass('Cb_Doctrine_Task_CbGenerateYamlDb');
     $cli->run($_SERVER['argv']);
