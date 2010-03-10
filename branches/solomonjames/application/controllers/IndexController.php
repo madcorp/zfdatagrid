421dcfac632062ca9e05bff026d3c14645242192
solomonjames <solomonjames@0f8c3b7c-9498-11dd-88c4-d55280a115f2>
2010-03-10 23:16:58 +0000

    Doctrine

--- branches/solomonjames/application/controllers/IndexController.php
+++ branches/solomonjames/application/controllers/IndexController.php
@@ -8,23 +8,24 @@ class IndexController extends Zend_Controller_Action
                                      ->from('Model_Country co')
                                      ->leftJoin('co.City ci');
                                      
-        $grid = $this->_getGrid();
-        $grid->setSource(new Bvb_Grid_Source_Doctrine($q));
+        $grid = $this->_getGrid($q);
         
         $this->view->grid = $grid->deploy();
     }
     
-    protected function _getGrid()
+    protected function _getGrid($query)
     {
         $config = new Zend_Config_Ini(AP . '/configs/grid.ini', AE);
         $grid = Bvb_Grid_Data::factory('Bvb_Grid_Deploy_Table', $config);
         
+        $grid->setSource(new Bvb_Grid_Source_Doctrine($query));
+        
         $grid->setEscapeOutput(false);
         $grid->setGridColumns(array('co_code', 'co_name', 'co_poop', 'ci_name'));
         $grid->updateColumn('ci_name', array('title' => 'City Name'));
+        $grid->imagesUrl = '/images/';
         //$grid->addTemplateDir('My/Template/Table', 'My_Template_Table', 'table');
         //$grid->addFormatterDir('My/Formatter', 'My_Formatter');
-        $grid->imagesUrl = '/images/';
         //$grid->cache = array('use' => 0, 'instance' => Zend_Registry::get('cache'), 'tag' => 'grid');
         
         return $grid;
