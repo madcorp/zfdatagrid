91213d0e8d42a8e14e4413c83d977133dcdd92f0
James Solomon <solomonjames@users.noreply.github.com>
2010-03-12 22:05:35 +0000

    All working, except CRUD

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,10 +4,18 @@ class IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
-        $q = Doctrine_Query::create()->select('co.code, co.name, co.continent AS poop, ci.name')
-                                     ->from('Model_Country co')
-                                     ->leftJoin('co.City ci');
+        //$q = Doctrine_Query::create()->select('co.code AS code, co.name AS country_name, co.continent AS poop, ci.name AS city_name')
+        //                             ->from('Model_Country AS co, co.City AS ci');
                                      
+//        $q = Doctrine_Query::create()->select('co.code AS code, co.name AS country_name, co.continent AS poop, ci.name AS city_name')
+//                                     ->from('Model_Country AS co')
+//                                     ->leftJoin('co.City AS ci');
+                                     
+//        $q = Doctrine_Query::create()->from('Model_Country AS co')
+//                                     ->leftJoin('co.City AS ci');
+
+        $q = Doctrine_Query::create()->from('Model_Country');
+        
         $grid = $this->_getGrid($q);
         
         $this->view->grid = $grid->deploy();
@@ -21,9 +29,15 @@ class IndexController extends Zend_Controller_Action
         $grid->setSource(new Bvb_Grid_Source_Doctrine($query));
         
         $grid->setEscapeOutput(false);
-        $grid->setGridColumns(array('co_code', 'co_name', 'co_poop', 'ci_name'));
-        $grid->updateColumn('ci_name', array('title' => 'City Name'));
+        //$grid->setGridColumns(array('code', 'country_name', 'poop', 'city_name'));
+        //$grid->updateColumn('city_name', array('title' => 'City Name'));
+        
+        //$grid->setGridColumns(array('co_code', 'co_name', 'co_continent', 'ci_name'));
+        
+        $grid->setGridColumns(array('code', 'name', 'continent'));
+        
         $grid->imagesUrl = '/images/';
+        $grid->setDetailColumns();
         //$grid->addTemplateDir('My/Template/Table', 'My_Template_Table', 'table');
         //$grid->addFormatterDir('My/Formatter', 'My_Formatter');
         //$grid->cache = array('use' => 0, 'instance' => Zend_Registry::get('cache'), 'tag' => 'grid');
